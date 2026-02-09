ea5b9fafb9f61d535ada6b916661062e18d5a890
amiralps <farzanetoosi001@gmail.com>
2026-02-09 23:24:55 +0330

    downgrade to stable next 16.1.6

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  // reactCompiler: true,
+  reactCompiler: true,
   // serverExternalPackages: ["next-auth"],
 
-  reactStrictMode: true,
+  // reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
